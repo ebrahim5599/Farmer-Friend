3659bcae870d7aab5d56b60b13eafb0770c1f6f8
ebrahim5599 <studyAndroid2020>
2021-10-21 16:43:43 +0200

    Graduation Project

--- app/src/main/java/com/graduation/farmerfriend/ui/MainActivity.java
+++ app/src/main/java/com/graduation/farmerfriend/ui/MainActivity.java
@@ -45,5 +45,6 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavigationUI.setupActionBarWithNavController(this, navCo, appBarConfiguration);
 
+        // Hi .
     }
 }
