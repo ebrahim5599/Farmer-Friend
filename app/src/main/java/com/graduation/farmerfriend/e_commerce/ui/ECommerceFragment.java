5fd09ff88904028f05c23fe92aee23c2b3a0b402
ahmed <ahmedward-eng>
2021-11-11 15:35:58 +0200

    home fragment components

--- app/src/main/java/com/graduation/farmerfriend/e_commerce/ui/ECommerceFragment.java
+++ app/src/main/java/com/graduation/farmerfriend/e_commerce/ui/ECommerceFragment.java
@@ -1,9 +1,9 @@
 package com.graduation.farmerfriend.e_commerce.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,47 +13,61 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.graduation.farmerfriend.R;
-import com.graduation.farmerfriend.e_commerce.Categorie;
+import com.graduation.farmerfriend.databinding.FragmentECommerceBinding;
+import com.graduation.farmerfriend.e_commerce.Category;
 import com.graduation.farmerfriend.e_commerce.Data;
 import com.graduation.farmerfriend.e_commerce.ViewRecycleCategoriesAdapter;
 import com.graduation.farmerfriend.e_commerce.ViewRecycleProductsAdapter;
+import com.graduation.farmerfriend.e_commerce.ui.cart.CartActivity;
+import com.graduation.farmerfriend.e_commerce.ui.deals.DealsActivity;
 
 
 public class ECommerceFragment extends Fragment {
-
-    RecyclerView recycler_seeds,recycler_tools ,recycler_machines ,recycler_fertilizers , recycler_categorie ;
+    FragmentECommerceBinding binding;
+    RecyclerView recycler_seeds,recycler_tools ,recycler_machines ,recycler_fertilizers , recycler_category;
     ImageView cart ;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_e_commerce, container, false);
+        binding = FragmentECommerceBinding.inflate(inflater,container,false);
+        View view = binding.getRoot();
 
         cart = view.findViewById(R.id.Shopping_Car);
-        recycler_categorie = view.findViewById(R.id.categorie_recycle);
+        recycler_category = view.findViewById(R.id.categorie_recycle);
         recycler_seeds = view.findViewById(R.id.recycleview_seeds);
         recycler_fertilizers = view.findViewById(R.id.recycleview_fertilizers);
         recycler_tools = view.findViewById(R.id.recycleview_tools);
         recycler_machines = view.findViewById(R.id.recycleview_machines);
 
-        cart.setOnClickListener(new View.OnClickListener() {
+        binding.toolsView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.fragmentContainerView,new Cart()).addToBackStack(null).commit();
+                Intent intent = new Intent(getContext(), DealsActivity.class);
+                startActivity(intent);
             }
         });
 
-        Categorie[] categorie ={
-                new Categorie("Seeds"),
-                new Categorie("Fertilizers"),
-                new Categorie("Tools"),
-                new Categorie("Machine Shops")
+        cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), CartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        Category[] category ={
+                new Category("Seeds"),
+                new Category("Fertilizers"),
+                new Category("Tools"),
+                new Category("Machine Shops")
         };
 
         LinearLayoutManager linearc = new LinearLayoutManager(getContext());
-        recycler_categorie.setLayoutManager((new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false)));
-        ViewRecycleCategoriesAdapter recycleViewAdapterc = new ViewRecycleCategoriesAdapter(getContext(),categorie);
-        recycler_categorie.setAdapter(recycleViewAdapterc);
+        recycler_category.setLayoutManager((new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false)));
+        ViewRecycleCategoriesAdapter recycleViewAdapterc = new ViewRecycleCategoriesAdapter(getContext(),category);
+        recycler_category.setAdapter(recycleViewAdapterc);
 
 
         Data[] data_seeds = {
